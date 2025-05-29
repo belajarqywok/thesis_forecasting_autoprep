85d97f8bd1a202c5eb3126381db1a341c57d01b6
belajarqywok <belajarqywok@gmail.com>
2025-05-29 13:15:41 +0700

    feat: create GA configuration

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from pandas import DataFrame
 from argparse import ArgumentParser, Namespace
 
-from stock_sorting.sorter_cythonize import Sorter
+from stock_sorting.sorter import Sorter
 from stock_scraping.historical_scraper_cythonize import HistoricalScraper
-from stock_scraping.infographic_scraper_cythonize import InfographicScraper
+from stock_scraping.infographic_scraper import InfographicScraper
 from stock_indicator.technical_indicator_cythonize import TechnicalIndicator
 
 from settings.logging_rules import logger
